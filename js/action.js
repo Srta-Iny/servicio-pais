bd029d44c3ce0724a95925a3e56af3e57696ca2b
brecabal <belenrecabal@gmail.com>
2016-12-02 12:36:20 -0300

    creanldo carousel estilos de esa seccion historias

--- js/action.js
+++ js/action.js
@@ -2,3 +2,17 @@
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
   });
+
+  $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+ 
+ // Next slide
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+// Set to nth slide
+$('.carousel').carousel('set', 4);
+      
